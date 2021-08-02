1b775fac27dd09d1fbd6eeeb2ba5a890cf07628b
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-08-02 18:56:24 -0300

    requisito 2 feito e aprovado no lint

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,9 @@
 import React from 'react';
+import Login from './Login';
 
 class Home extends React.Component {
   render() {
-    return (
-      <fieldset>
-        <label htmlFor="email-input">
-          Email:
-          <input data-testid="email-input" type="email" />
-        </label>
-        <label htmlFor="password-input">
-          Senha:
-          <input data-testid="password-input" />
-        </label>
-        <button
-          type="button"
-        >
-          Entrar
-        </button>
-      </fieldset>
-    );
+    return <Login />;
   }
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,65 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      buttonDisable: true,
+      email: '',
+      password: '',
+    };
+    this.changeInput = this.changeInput.bind(this);
+  }
+
+  changeInput(e) {
+    if (e.target.name === 'email') {
+      const valor = e.target.value;
+      this.setState({
+        email: valor,
+      });
+    } else if (e.target.name === 'password') {
+      const valor = e.target.value;
+      this.setState({
+        password: valor,
+      });
+    }
+    const { email, password } = this.state;
+    const regex = /^[a-z0-9_.-]+@[a-z]+\.[a-z]{2,3}(?:\.[a-z]{2})?$/;
+    if (regex.test(email) && password.length > 2 + 2) {
+      this.setState({ buttonDisable: false });
+    }
+  }
+
   render() {
-    return <div>Login</div>;
+    const { email, password, buttonDisable } = this.state;
+    return (
+      <fieldset>
+        <label htmlFor="email-input">
+          Email:
+          <input
+            name="email"
+            data-testid="email-input"
+            onChange={ this.changeInput }
+            value={ email }
+          />
+        </label>
+        <label htmlFor="password-input">
+          Senha:
+          <input
+            name="password"
+            data-testid="password-input"
+            onChange={ this.changeInput }
+            value={ password }
+          />
+        </label>
+        <button
+          type="button"
+          disabled={ buttonDisable }
+        >
+          Entrar
+        </button>
+      </fieldset>
+    );
   }
 }
 
