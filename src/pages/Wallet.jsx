8a173b411f8425705a998b85a7adc35457218528
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-08-09 03:18:21 -0300

    requisito 6 feito e aprovado no lint

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -5,17 +5,49 @@ import { PropTypes } from 'prop-types';
 class Wallet extends React.Component {
   render() {
     const { email } = this.props;
+    const pagamento = ['Dinheiro', 'Cartão de Crédito', 'Cartão de Débito'];
+    const tag = ['Alimentação', 'Lazer', 'Trabalho', 'Transporte', 'Saúde'];
     return (
       <>
         <header>
           <p data-testid="email-field">{ email }</p>
           <p data-testid="total-field">
-            Despesa total:
+            Despesa total
             {0}
           </p>
           <p data-testid="header-currency-field">BRL</p>
         </header>
-        <div>TrybeWallet</div>
+        <fieldset>
+          <label htmlFor="valor">
+            Valor
+            <input id="valor" type="text" />
+          </label>
+          <label htmlFor="descricao">
+            Descrição
+            <input id="descricao" type="text" />
+          </label>
+          <label htmlFor="moeda">
+            Moeda
+            <select id="moeda">
+              <option>
+                BRL
+              </option>
+            </select>
+          </label>
+          <label htmlFor="metodo-pagamento">
+            Método de pagamento
+            <select id="metodo-pagamento">
+              {pagamento
+                .map((item, index) => <option key={ index }>{item}</option>)}
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select id="tag">
+              {tag.map((item, index) => <option key={ index }>{ item }</option>)}
+            </select>
+          </label>
+        </fieldset>
       </>
     );
   }
